4944aa6b20c0a07b18231dcb2267521baac2cb65
MyznessMC <Myzness@nukkit.cc>
2025-01-30 16:01:32 +0800

    UploadVersion
    UploadVersion

--- src/cc/nukkit/myzness/auction/Main.java
+++ src/cc/nukkit/myzness/auction/Main.java
@@ -1,6 +1,10 @@
 package cc.nukkit.myzness.auction;
 
 import cn.nukkit.Player;
+import cn.nukkit.form.element.ElementInput;
+import cn.nukkit.form.element.ElementLabel;
+import cn.nukkit.form.handler.FormResponseHandler;
+import cn.nukkit.form.window.FormWindowCustom;
 import cn.nukkit.Server;
 import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
@@ -11,14 +15,17 @@ import cn.nukkit.utils.Config;
 import me.onebone.economyapi.EconomyAPI;
 import cn.nukkit.item.Item;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class Main extends PluginBase {
     private Map<String, Auction> auctions = new HashMap<>();
     private Config config;
+    private Config languageConfig; // 添加语言配置
 
     @Override
     public void onEnable() {
@@ -26,14 +33,22 @@ public class Main extends PluginBase {
         saveDefaultConfig();
         config = getConfig();
 
+        // 复制 Language.yml 和 Log.json 到配置文件夹
+        saveResource("Language.yml", false);
+        saveResource("Log.json", false);
+
+        // 加载 Language.yml 文件
+        languageConfig = new Config(new File(getDataFolder(), "Language.yml"), Config.YAML); // 添加语言配置加载
+
         // 注册命令
         registerCommands();
 
         this.getLogger().info("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
         this.getLogger().info(" ");
-        this.getLogger().info("Auction 拍卖");
-        this.getLogger().info("author: 眠悠子Myzness");
+        this.getLogger().info("AuctionSystem 拍卖系统");
+        this.getLogger().info("author: 眠悠子Miyoz");
         this.getLogger().info("这是一个免费插件，如果你花钱了，那你一定是被骗了");
+        this.getLogger().info("你可以访问MOT社区获取最新版插件:https://bbs.nukkit-mot.com");
         this.getLogger().info(" ");
         this.getLogger().info("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
     }
@@ -58,7 +73,7 @@ public class Main extends PluginBase {
 
             // 检查是否为控制台发送的指令
             if (!(sender instanceof Player)) {
-                sender.sendMessage("§c请勿在控制台执行该指令!");
+                sender.sendMessage(languageConfig.getString("console_error")); // 使用语言配置
                 return true;
             }
 
@@ -71,21 +86,82 @@ public class Main extends PluginBase {
                     return handleJoin(player, args);
                 case "cancel":
                     return handleCancel(player, args);
+                case "setting":
+                    // 检查玩家是否为 OP
+                    if (!player.isOp()) {
+                        player.sendMessage(languageConfig.getString("no_permission")); // 使用语言配置
+                        return true;
+                    }
+                    return handleSetting(player, args);
+                case "reload": // 新增 reload 子命令
+                    // 检查玩家是否为 OP
+                    if (!player.isOp()) {
+                        player.sendMessage(languageConfig.getString("no_permission")); // 使用语言配置
+                        return true;
+                    }
+                    reload(); // 调用 reloadConfig 方法
+                    player.sendMessage("§a" + languageConfig.getString("config_reloaded")); // 使用语言配置
+                    return true;
                 default:
-                    player.sendMessage("§c未知命令，请输入 /auction help 查看帮助!");
+                    player.sendMessage(languageConfig.getString("unknown_command")); // 使用语言配置
                     return true;
             }
         }
         return false;
     }
 
+    // 添加 handleSetting 方法
+    private boolean handleSetting(Player player, String[] args) {
+        FormWindowCustom form = new FormWindowCustom(languageConfig.getString("setting_title"));
+        // 添加组件
+        form.addElement(new ElementInput(languageConfig.getString("setting_auction_time_title"), "60"));
+        form.addElement(new ElementInput(languageConfig.getString("setting_broadcast_time_title"), "10"));
+        form.addElement(new ElementInput(languageConfig.getString("setting_broadcast_content_title"), "§e{player}§a开启了一个拍卖!物品为§e{item} §b({id}:{special})§a，起拍价为§e {price}§a，输入 §e/auction join {name} <金额>§a 来参与吧，§c在 §e{time}§c 秒后拍卖就将结束了哦"));
+        form.addElement(new ElementLabel(languageConfig.getString("setting_broadcast_tips")));
+        // 设置提交操作
+        form.addHandler(FormResponseHandler.withoutPlayer(ignored -> {
+            if (form.wasClosed()) {
+                return;
+            }
+
+            try {
+                // 获取表单中的输入值
+                String auctionTimeInput = form.getResponse().getInputResponse(0); // 获取 auction_time 输入框的值
+                String broadcastTimeInput = form.getResponse().getInputResponse(1); // 获取 broadcast_time 输入框的值
+                String broadcastContentInput = form.getResponse().getInputResponse(2); // 获取 broadcast_content 输入框的值
+
+                int auctionTime = Integer.parseInt(auctionTimeInput);
+                int broadcastTime = Integer.parseInt(broadcastTimeInput);
+
+                // 修改配置文件中的 auction_time, broadcast_time 和 broadcast_content
+                config.set("auction_time", auctionTime);
+                config.set("broadcast_time", broadcastTime);
+                config.set("broadcast_content", broadcastContentInput);
+
+                // 保存配置文件
+                config.save();
+
+                // 提示指令执行者
+                player.sendMessage("§a" + languageConfig.getString("settings_saved")); // 使用语言配置
+            } catch (NumberFormatException e) {
+                player.sendMessage("§c" + languageConfig.getString("invalid_number")); // 使用语言配置
+            }
+        }));
+        // 显示表单给玩家
+        player.showFormWindow(form);
+        return true;
+    }
+
+
     // 显示帮助信息
     private void showHelp(CommandSender sender) {
-        sender.sendMessage("§6===== 拍卖指令帮助 =====");
-        sender.sendMessage("§a/auction sell <起拍价> <拍卖名> - 发起拍卖");
-        sender.sendMessage("§a/auction join <拍卖名> <出价> - 参与拍卖");
-        sender.sendMessage("§a/auction cancel <拍卖名> - 取消拍卖");
-        sender.sendMessage("§a/auction help - 查看指令帮助");
+        sender.sendMessage("§6===== " + languageConfig.getString("help_title") + " =====");
+        sender.sendMessage("§a/auction sell <price> <name> - " + languageConfig.getString("help_sell"));
+        sender.sendMessage("§a/auction join <name> <price> - " + languageConfig.getString("help_join"));
+        sender.sendMessage("§a/auction cancel <name> - " + languageConfig.getString("help_cancel"));
+        sender.sendMessage("§a/auction setting - " + languageConfig.getString("help_setting"));
+        sender.sendMessage("§a/auction reload - " + languageConfig.getString("help_reload"));
+        sender.sendMessage("§a/auction help - " + languageConfig.getString("help_help"));
         sender.sendMessage("§6========================");
     }
 
@@ -94,7 +170,7 @@ public class Main extends PluginBase {
 
         // 检查卖家是否已有拍卖活动进行中
         if (activeSellers.contains(seller.getName())) {
-            seller.sendMessage("你已经有一个拍卖活动正在进行，不能创建新的拍卖!");
+            seller.sendMessage(languageConfig.getString("already_active")); // 使用语言配置
             return true;
         }
 
@@ -102,21 +178,21 @@ public class Main extends PluginBase {
         try {
             startingPrice = Double.parseDouble(args[1]);
         } catch (NumberFormatException e) {
-            seller.sendMessage("起拍价必须是一个数字！");
+            seller.sendMessage(languageConfig.getString("invalid_price")); // 使用语言配置
             return true;
         }
 
         String auctionName = args[2];
 
         if (auctions.containsKey(auctionName)) {
-            seller.sendMessage("该拍卖活动已经存在!");
+            seller.sendMessage(languageConfig.getString("auction_exists")); // 使用语言配置
             return true;
         }
 
         // 获取卖家手上的物品
         Item heldItem = seller.getInventory().getItemInHand();
         if (heldItem == null || heldItem.getId() == 0) {
-            seller.sendMessage("你没有持有物品进行拍卖!");
+            seller.sendMessage(languageConfig.getString("no_item")); // 使用语言配置
             return true;
         }
         seller.getInventory().remove(heldItem); // 移除卖家的拍卖物品
@@ -129,7 +205,7 @@ public class Main extends PluginBase {
         activeSellers.add(seller.getName());
 
         auction.start();
-        seller.sendMessage("§a拍卖活动 §e" + auctionName + "§a 已创建，起拍价为 §e" + startingPrice + "§a!");
+        seller.sendMessage("§a" + languageConfig.getString("auction_created").replace("{auctionName}", auctionName).replace("{startingPrice}", String.valueOf(startingPrice))); // 使用语言配置
         return true;
     }
 
@@ -141,24 +217,24 @@ public class Main extends PluginBase {
         try {
             bidAmount = Double.parseDouble(args[2]);
         } catch (NumberFormatException e) {
-            bidder.sendMessage("§c出价必须是一个数字！");
+            bidder.sendMessage("§c" + languageConfig.getString("auction_must_number")); // 这里暂时保留，因为没有对应的语言配置
             return true;
         }
 
         Auction auction = auctions.get(auctionName);
         if (auction == null) {
-            bidder.sendMessage("§c该拍卖活动不存在!");
+            bidder.sendMessage("§c" + languageConfig.getString("auction_not_found").replace("{auctionName}", auctionName)); // 使用语言配置
             return true;
         }
 
         auction.join(bidder.getName(), bidAmount);
-        bidder.sendMessage("§a你已参与拍卖 §e" + auctionName + "§a，出价为 §e" + bidAmount + "§a!");
+        bidder.sendMessage("§a" + languageConfig.getString("bid_placed").replace("{auctionName}", auctionName).replace("{bidAmount}", String.valueOf(bidAmount))); // 使用语言配置
         return true;
     }
 
     private boolean handleCancel(Player player, String[] args) {
         if (args.length < 2) {
-            player.sendMessage("§c请提供拍卖名!");
+            player.sendMessage(languageConfig.getString("provide_auction_name")); // 使用语言配置
             return false;
         }
 
@@ -166,13 +242,13 @@ public class Main extends PluginBase {
         Auction auction = auctions.get(auctionName);
 
         if (auction == null) {
-            player.sendMessage("§c拍卖活动 §e" + auctionName + "§c 不存在!");
+            player.sendMessage("§c" + languageConfig.getString("auction_not_found").replace("{auctionName}", auctionName)); // 使用语言配置
             return true;
         }
 
         // 确认当前玩家是否为拍卖发起者或具有管理员权限
         if (!auction.getSeller().equals(player.getName()) && !player.isOp() && !player.hasPermission("auction.cancel.admin")) {
-            player.sendMessage("§c你没有权限取消此拍卖活动!");
+            player.sendMessage(languageConfig.getString("no_permission")); // 使用语言配置
             return true;
         }
 
@@ -182,9 +258,27 @@ public class Main extends PluginBase {
         // 从拍卖列表中移除
         auctions.remove(auctionName);
 
-        player.sendMessage("§a拍卖活动 §e" + auctionName + "§a 已取消!");
+        player.sendMessage("§a" + languageConfig.getString("auction_cancelled").replace("{auctionName}", auctionName)); // 使用语言配置
+
+        // 添加日志记录
+        logTransaction(auctionName, auction.getSeller(), null, auction.getCurrentBid(), "Cancelled");
 
         return true;
+    }
+
+    // 添加日志记录方法
+    private void logTransaction(String auctionName, String seller, String buyer, double price, String status) {
+        try (FileWriter file = new FileWriter(getDataFolder() + File.separator + "Log.json", true)) {
+            // 获取当前时间
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            String timestamp = now.format(formatter);
+
+            String logEntry = String.format("{\"auctionName\": \"%s\", \"seller\": \"%s\", \"buyer\": \"%s\", \"price\": %.2f, \"status\": \"%s\", \"timestamp\": \"%s\"},\n", auctionName, seller, buyer, price, status, timestamp);
+            file.write(logEntry);
+        } catch (IOException e) {
+            getLogger().warning("Failed to write to Log.json: " + e.getMessage());
+        }
     }
 
     private class Auction {
@@ -239,7 +333,7 @@ public class Main extends PluginBase {
             if (bidder.equals(seller.getName())) {
                 Player player = Server.getInstance().getPlayer(bidder);
                 if (player != null) {
-                    player.sendMessage("§c你不能参与自己发起的拍卖!");
+                    player.sendMessage(languageConfig.getString("self_bid_error")); // 使用语言配置
                 }
                 return;
             }
@@ -249,7 +343,7 @@ public class Main extends PluginBase {
                 // 出价低于当前最高价或者低于起拍价
                 Player player = Server.getInstance().getPlayer(bidder);
                 if (player != null) {
-                    player.sendMessage("§c你的出价必须高于当前最高价且不低于起拍价！");
+                    player.sendMessage(languageConfig.getString("bid_too_low")); // 使用语言配置
                 }
                 return;
             }
@@ -259,7 +353,7 @@ public class Main extends PluginBase {
             if (balance < bid) {
                 Player player = Server.getInstance().getPlayer(bidder);
                 if (player != null) {
-                    player.sendMessage("§c你的余额不足以进行此出价！");
+                    player.sendMessage(languageConfig.getString("insufficient_balance")); // 使用语言配置
                 }
                 return;
             }
@@ -275,7 +369,7 @@ public class Main extends PluginBase {
             EconomyAPI.getInstance().reduceMoney(bidder, bid);
 
             // 广播当前出价情况
-            Server.getInstance().broadcastMessage("§e" + bidder + "§a 以 §e" + bid + " §a的价格出价。");
+            Server.getInstance().broadcastMessage("§e" + languageConfig.getString("bid_broadcast").replace("{bidAmount}", String.valueOf(bid))); // 使用语言配置
         }
 
 
@@ -288,16 +382,25 @@ public class Main extends PluginBase {
                 Player winner = plugin.getServer().getPlayer(currentBidder);
                 if (winner != null) {
                     winner.getInventory().addItem(auctionItem); // 将物品转给赢家
-                    seller.sendMessage("§e" + currentBidder + "§a 赢得了你的拍卖!");
+                    seller.sendMessage("§e" + currentBidder + "§a " + languageConfig.getString("auction_won")); // 使用语言配置
                     // 给卖家增加当前竞拍金额
                     EconomyAPI.getInstance().addMoney(seller, currentBid);
+
+                    // 添加日志记录
+                    logTransaction(name, seller.getName(), currentBidder, currentBid, "Successful");
                 } else {
-                    seller.sendMessage("§c赢家不在服务器上!");
+                    seller.sendMessage(languageConfig.getString("winner_offline")); // 使用语言配置
+
+                    // 添加日志记录
+                    logTransaction(name, seller.getName(), currentBidder, currentBid, "WinnerOffline");
                 }
             } else {
                 // 拍卖未成交，物品返还给卖家
-                seller.sendMessage("§c拍卖未成交!");
+                seller.sendMessage(languageConfig.getString("auction_unsuccessful")); // 使用语言配置
                 seller.getInventory().addItem(auctionItem); // 将拍卖的物品返还给卖家
+
+                // 添加日志记录
+                logTransaction(name, seller.getName(), null, startingPrice, "Unsuccessful");
             }
 
             // 从拍卖列表中移除
@@ -331,30 +434,28 @@ public class Main extends PluginBase {
             plugin.activeSellers.remove(seller.getName());
         }
 
-
         // 广播当前拍卖状态
         private void broadcastAuction(Main plugin) {
-            // 获取 broadcast_content 模板
             String broadcastTemplate = plugin.config.getString("broadcast_content");
 
-            // 计算剩余时间
             long currentTime = System.currentTimeMillis();
-            long timeLeft = (endTime - currentTime) / 1000;  // 剩余时间，单位为秒
+            long timeLeft = (endTime - currentTime) / 1000;
 
-            // 替换模板中的占位符
             String broadcastMessage = broadcastTemplate
                     .replace("{player}", seller.getName())
                     .replace("{item}", auctionItem.getName())
                     .replace("{id}", String.valueOf(auctionItem.getId()))
-                    .replace("{special}", String.valueOf(auctionItem.getDamage())) // damage 表示物品的特殊值
+                    .replace("{special}", String.valueOf(auctionItem.getDamage()))
                     .replace("{price}", String.valueOf(startingPrice))
                     .replace("{name}", name)
-                    .replace("{time}", String.valueOf(timeLeft));  // 添加剩余时间
+                    .replace("{time}", String.valueOf(timeLeft));
 
-            // 广播消息
             plugin.getServer().broadcastMessage(broadcastMessage);
         }
 
+        public double getCurrentBid() {
+            return currentBid;
+        }
 
         public void start() {
         }
@@ -366,4 +467,9 @@ public class Main extends PluginBase {
 
     private Set<String> activeSellers = new HashSet<>();
 
+    private void reload() {
+        saveDefaultConfig();
+        config = getConfig();
+        languageConfig = new Config(new File(getDataFolder(), "Language.yml"), Config.YAML); // 重新加载语言配置
+    }
 }
